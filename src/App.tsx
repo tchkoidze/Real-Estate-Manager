4be5c523f838331e1066923cf2059a9a8efa0789
tchkoidze <ilia.chkoidze12@gmail.com>
2024-09-21 19:00:07 +0400

    refactor: delete app.css file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Listings from "./pages/Listings";
 import AddListing from "./pages/AddListing";
